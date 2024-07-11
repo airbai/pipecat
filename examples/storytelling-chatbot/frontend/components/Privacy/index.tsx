43c6074b2a38284c9239e8f7473ec032fd6be623
星火小程序 <baipengfei@gmail.com>
2024-07-11 15:00:14 +0800

    privacy for window.ai

---
+++ examples/storytelling-chatbot/frontend/components/Privacy/index.tsx
@@ -0,0 +1,16 @@
+
+export const Privacy: React.FC = () => {
+  return (
+    <>
+      <div className="vol bg-teal-700" />
+      <p>
+      No user data will be collected by this Extension.<br></br>
+      Thanks.<br></br>
+
+      Contact via dothingsatonce@gmail.com
+      </p>
+    </>
+  );
+};
+
+export default Privacy;
